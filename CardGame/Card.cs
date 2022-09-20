1a06fca01acf3e4bdc1337e13bfe8195761bc48e
12den24 <12den24@gmail.com>
2022-09-20 21:44:06 +0300

    Добавьте файлы проекта.

---
+++ CardGame/Card.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace CardGame
+{
+    internal class Card
+    {
+        byte suit;
+        bool color;
+        string rank;
+
+        public Card(byte suit, bool color, string rank)
+        {
+            this.suit = suit;
+            this.color = color;
+            this.rank = rank;
+        }
+
+        public void Show()
+        {
+            if (color)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Black;
+            }
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.Write($"[{(char)suit}{rank}]");
+            Console.ResetColor();
+        }
+    }
+}
---
+++ CardGame/Player.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CardGame
+{
+    internal class Player
+    {
+        List<Card> cards;
+
+        public Player(List<Card> cards)
+        {
+            this.cards = cards;
+        }
+
+        public void ShowCards()
+        {
+            foreach (Card card in cards)
+            {
+                card.Show();
+                Console.Write(" ");
+            }
+        }
+    }
+}
